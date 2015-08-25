c2395979a57f84e4707ebaa09d3bd88e35abb531
Felipe Knorr Kuhn <git@knorrium.info>
2015-08-24 22:37:08 -0700

    fix the movies contract class

--- app/src/main/java/knorrium/info/popularmovies/data/MoviesContract.java
+++ app/src/main/java/knorrium/info/popularmovies/data/MoviesContract.java
@@ -5,39 +5,33 @@ import android.content.ContentUris;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
-/**
- * Created by felipek on 8/23/15.
- */
 public class MoviesContract {
 
-    public static final String CONTENT_AUTHORITY = "info.knorrium.popularmovies";
+    public static final String CONTENT_AUTHORITY = "knorrium.info.popularmovies";
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
     public static final String PATH_MOVIES = "movies";
-    public static final String PATH_MOVIE = "movies";
-
 
     public static final class MovieEntry implements BaseColumns {
 
         public static final Uri CONTENT_URI =
-                BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIE).build();
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIES).build();
 
         public static final String CONTENT_TYPE =
-                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE;
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIES;
 
-        public static final String CONTENT_ITEM_TYPE =
-                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE;
-
-        // Table name
         public static final String TABLE_NAME = "movies";
 
-        public static final String COLUMN_MOVIE_TITLE = "movie_title";
-        public static final String COLUMN_MOVIE_OVERVIEW = "movie_overview";
-        public static final String COLUMN_MOVIE_POSTER = "movie_poster";
-        public static final String COLUMN_MOVIE_POPULARITY = "movie_popularity";
-        public static final String COLUMN_MOVIE_RATING = "movie_rating";
+        public static final String COLUMN_MOVIE_ID = "id";
+        public static final String COLUMN_MOVIE_TITLE = "title";
+        public static final String COLUMN_MOVIE_OVERVIEW = "overview";
+        public static final String COLUMN_MOVIE_POSTER = "poster_path";
+        public static final String COLUMN_MOVIE_POPULARITY = "popularity";
+        public static final String COLUMN_MOVIE_RATING = "vote_average";
+        public static final String COLUMN_MOVIE_RELEASE_DATE = "release_date";
 
-        public static Uri buildLocationUri(long id) {
+
+        public static Uri buildMovieUri(long id) {
             return ContentUris.withAppendedId(CONTENT_URI, id);
         }
     }
