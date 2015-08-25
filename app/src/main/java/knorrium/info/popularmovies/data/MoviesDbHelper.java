92dbc37ec094fa66fc2e5196d8025a03e0386e57
Felipe Knorr Kuhn <git@knorrium.info>
2015-08-24 22:37:19 -0700

    update the database schema

--- app/src/main/java/knorrium/info/popularmovies/data/MoviesDbHelper.java
+++ app/src/main/java/knorrium/info/popularmovies/data/MoviesDbHelper.java
@@ -4,9 +4,6 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-/**
- * Created by felipek on 8/23/15.
- */
 public class MoviesDbHelper extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1;
@@ -20,10 +17,12 @@ public class MoviesDbHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
 
         final String SQL_CREATE_MOVIE_TABLE = "CREATE TABLE " + MoviesContract.MovieEntry.TABLE_NAME + " (" +
-                MoviesContract.MovieEntry._ID + " INTEGER PRIMARY KEY," +
-                MoviesContract.MovieEntry.COLUMN_MOVIE_TITLE + " TEXT UNIQUE NOT NULL, " +
+                MoviesContract.MovieEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                MoviesContract.MovieEntry.COLUMN_MOVIE_ID + " INTEGER NOT NULL, " +
+                MoviesContract.MovieEntry.COLUMN_MOVIE_TITLE + " TEXT NOT NULL, " +
                 MoviesContract.MovieEntry.COLUMN_MOVIE_OVERVIEW + " TEXT NOT NULL, " +
                 MoviesContract.MovieEntry.COLUMN_MOVIE_POSTER + " TEXT NOT NULL, " +
+                MoviesContract.MovieEntry.COLUMN_MOVIE_RELEASE_DATE + " TEXT NOT NULL, " +
                 MoviesContract.MovieEntry.COLUMN_MOVIE_POPULARITY + " REAL NOT NULL, " +
                 MoviesContract.MovieEntry.COLUMN_MOVIE_RATING + " REAL NOT NULL " +
                 " );";
